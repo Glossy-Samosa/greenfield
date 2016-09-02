96916b8347ff0389835a161614b73b2631eb6b9d
Joseph Yu <joeyu629@gmail.com>
2016-09-01 19:50:27 -0700

    (feat) function to get directions using google maps api

--- server/server.js
+++ server/server.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var request = require('request');
+var GoogleMapsLoader = require('google-maps');
 
 var app = express();
+
+// load google maps stuffs
+GoogleMapsLoader.load(function(google) {
+  new google.maps.Map(el, options);
+});
+
+GoogleMapsLoader.onLoad(function() {
+  console.log('We just loaded Google Maps API!');
+});
+GoogleMapsLoader.LIBRARIES = ['geometry', 'places'];
+
+GoogleMapsLoader.KEY = window.google_maps_API_KEY;
 
 // open mongoose connection
 mongoose.connect('mongodb://localhost/uberEco');
