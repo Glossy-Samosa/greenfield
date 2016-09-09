fc0e73ae132a46d71dea81bc345839e2c9af4b9f
Joseph Yu <joeyu629@gmail.com>
2016-09-09 12:01:43 -0700

    fix redirect to send object instead with location prop

--- server/auth/authUser.js
+++ server/auth/authUser.js
@@ -3,7 +3,7 @@
 var authenticateUser = function(req, res, next) {
   if (!req.user) {
     // if the user doesn't have a session, gtfo
-    return res.redirect('/login');
+    return res.status(418).send({ location: '/auth/login' });
   } else {
     next();
   }
